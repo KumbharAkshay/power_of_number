4ce26536e28e5eab3b6d9637497d661b09dc01d0
KumbharAkshay <32314015+KumbharAkshay@users.noreply.github.com>
2019-12-23 13:42:40 -0500

    Add files via upload

---
+++ PowerOfANumber.java
@@ -0,0 +1,19 @@
+class PowerOfANumber { 
+    static int power(int x, int y) 
+    { 
+        if (y == 0) 
+            return 1; 
+        else if (y % 2 == 0) 
+            return power(x, y / 2) * power(x, y / 2); 
+        else
+            return x * power(x, y / 2) * power(x, y / 2); 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        int x = 2; 
+        int y = 3; 
+  
+        System.out.println("%d", power(x, y)); 
+    } 
+} 
